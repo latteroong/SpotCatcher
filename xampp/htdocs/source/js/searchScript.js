b30f6f8acf146f1464bd007f0fea47e4d40797a4
임예은 <122524095+latteroong@users.noreply.github.com>
2024-09-19 14:29:49 +0900

    첫 화면 지도를 사진으로 변경

--- xampp/htdocs/source/js/searchScript.js
+++ xampp/htdocs/source/js/searchScript.js
@@ -1,4 +1,4 @@
-let locPosition = new kakao.maps.LatLng(0.0, 0.0);
+let locPosition = new kakao.maps.LatLng(33.450701, 126.570667);
 let detailAddr = ""
 let geocoder = new kakao.maps.services.Geocoder();
 let width = 500;
@@ -135,11 +135,9 @@ updateRemoveButtons();
 
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        center: locPosition, // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨 
-    }; 
-
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+};
 
 // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
 if (navigator.geolocation) {
@@ -153,15 +151,16 @@ if (navigator.geolocation) {
         locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
             message = '<div style="padding:5px;">현재 위치</div>'; // 인포윈도우에 표시될 내용입니다
         
-        // 마커와 인포윈도우를 표시합니다
-        displayMarker(locPosition, message);
+        // // 마커와 인포윈도우를 표시합니다
+        // displayMarker(locPosition, message);
         
         searchDetailAddrFromCoords(locPosition, function(result, status) {
             if (status === kakao.maps.services.Status.OK) {
                 detailAddr = !!result[0].road_address ? result[0].road_address.address_name:"";
             }
         });
-            
+
+        setMap(locPosition);
     });
     
 } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
@@ -169,7 +168,16 @@ if (navigator.geolocation) {
     locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
         message = 'geolocation을 사용할수 없어요..'
         
-    displayMarker(locPosition, message);
+    // displayMarker(locPosition, message);
+    setMap(locPosition);
+}
+
+function setMap(locPosi) {
+    const staticMap = new kakao.maps.StaticMap(mapContainer, 
+        mapOption = { 
+        center: locPosi, // 지도의 중심좌표
+        level: 2 // 지도의 확대 레벨 
+    });
 }
 
 // 지도에 마커와 인포윈도우를 표시하는 함수입니다
