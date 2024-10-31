801d132ef2c6509aaac2e7c6bd5f33bf0a8baf20
임예은 <122524095+latteroong@users.noreply.github.com>
2024-10-31 10:48:48 +0900

    제외

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,6 +1,8 @@
+const api = require("../common/apikey");
+
 const main = ((req, res) => {
     try {
-        res.render('index');
+        res.render('index', { kakaoApiKey: api.config.apikey});
     } catch (error) {
         res.status(500).send("<H1>500</H1> Error" + error);
     }
--- controller/resultController.js
+++ controller/resultController.js
@@ -1,11 +1,12 @@
 const common = require("../common/common");
+const api = require("../common/apikey");
 let dataset;
 
 const postResult = ((req, res) => {
     try {
         console.log(req.body);
         dataset = req.body
-        res.render('result', {data: dataset});
+        res.render('result', {data: dataset, kakaoApiKey: api.config.apikey});
     } catch (error) {
         res.status(500).send("<H1>500</H1> Error" + error);
     }
